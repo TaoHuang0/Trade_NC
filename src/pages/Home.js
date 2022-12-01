de9c5c1d3c5479e8532fa74d23dd5efa170de044
Bang Gong <gongbang@email.unc.edu>
2022-12-01 15:04:48 -0500

    Home.js added

---
+++ src/pages/Home.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { auth, db } from "../firebase-config";
+
+const Home = ({ isAuth }) => {
+  const [postLists, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+  const [searchSchool, setSearchSchool] = useState("All");
+  const [filteredLists, setFilteredLists] = useState([]);
+
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id);
+    await deleteDoc(postDoc);
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    document.title = "TradeNC";
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  }, []);
+
+  useEffect(()=> {
+    setFilteredLists(
+      postLists.filter(
+        (post) => 
+          //post.title.toLowerCase().includes(searchSchool.toLowerCase()) ||
+          //post.title.toLowerCase().includes(searchSchool.toLowerCase())
+          (searchSchool==="All") || (post.schoolName === searchSchool)       
+      )
+    );
+  })
+
+  return (
+    <div className="homePage">
+      <div className="selectSchoolHome">
+        <label>🏫 Your University:</label>
+        <select className="selectSchool" onChange={(e) => {
+            setSearchSchool(e.target.value)
+          }}>
+            <option value="All">All</option>
+            <option value="UNC - Chapel Hill">UNC - Chapel Hill</option>
+            <option value="Duke">Duke</option>
+            <option value="NCSU">NCSU</option>
+            <option value="NCCU">NCCU</option>
+          </select>
+
+      </div>
+      {filteredLists.map((post) => {
+        let imgUrl = post.imgRef;
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1> {post.title}</h1>
+              </div>
+              <div className="deletePost">
+                {isAuth && post.author.id === auth.currentUser.uid && (
+                  <button
+                    onClick={() => {
+                      deletePost(post.id);
+                    }}
+                  >
+                    {" "}
+                    <div className="closeIcon"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                    </svg></div>
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className="postTextContainer"> {post.postText} </div>
+            <div className="priceContainer"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-currency-dollar" viewBox="0 0 16 16">
+              <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z" />
+            </svg>{post.itemPrice}</div>
+            <div className="contactContainer">
+              <div>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chat-dots" viewBox="0 0 16 16">
+                <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z" />
+              </svg>
+              <span>&nbsp;Contact info: {post.contactInfo}</span></div><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" id="copyIcon" className="bi bi-clipboard" viewBox="0 0 16 16" cursor="pointer" onClick={() => {navigator.clipboard.writeText(post.contactInfo)}}>
+  <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+  <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+</svg>
+            </div>
+            <img className="postImage" src={imgUrl}></img>
+            <h3>@{post.author.name}</h3>
+            <h3>Posted on {post.myPostTime}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Home;
